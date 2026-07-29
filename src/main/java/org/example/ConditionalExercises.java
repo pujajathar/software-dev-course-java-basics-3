fa77c5660dc488d66ecdffdc03a2c3f536eee189
pujajathar <pujajathar91@gmail.com>
2026-07-28 21:08:23 -0500

    Completed java-basics-3 practice

--- src/main/java/org/example/ConditionalExercises.java
+++ src/main/java/org/example/ConditionalExercises.java
@@ -3,7 +3,7 @@ package org.example;
 public class ConditionalExercises {
     public boolean lessThanFive(int x) {
         // Replace the line below with code that returns true if x is less than 5, and false otherwise
-        // (use an if statement with an else block.)
+        if (x < 5) return true;
         return false;
     }
 
@@ -11,7 +11,9 @@ public class ConditionalExercises {
         // Replace the line below with code that returns "child" if age is less than 13,
         // "teen" if age is less than 20, and "adult" otherwise
         // (use if, else if, and else)
-        return "";
+        if (age < 13)  return "child";
+         else if (age < 20) return "teen";
+         else return "adult";
     }
 
     public boolean isValidPassword(String password) {
@@ -20,6 +22,7 @@ public class ConditionalExercises {
         // Replace the line below with code that returns true if password is at least 8 characters long
         // and false otherwise
         // (use an if statement with an else block.)
-        return false;
+        if (passwordLength >= 8) return true;
+         else return false;
     }
 }

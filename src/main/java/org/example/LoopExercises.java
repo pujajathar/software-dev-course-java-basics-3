48b8713d76c2e04e695be96162900ba371684536
pujajathar <pujajathar91@gmail.com>
2026-07-29 21:58:25 -0500

    Added main method in LoopExercises  file to make it runnable.

--- src/main/java/org/example/LoopExercises.java
+++ src/main/java/org/example/LoopExercises.java
@@ -1,5 +1,5 @@
 package org.example;
-
+import java.util.Scanner;
 public class LoopExercises {
     public int sum(int n) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
@@ -11,13 +11,13 @@ public class LoopExercises {
         return total;
     }
 
-    public int sumUntilEven(int n) {
+    public int sumUntilEven(int n1) {
         // Replace the line below with code that returns the sum of the numbers from 1 to n
         // but stops adding when the sum is even
         // (use a while loop with a sum variable and a counter variable)
         int sum = 0;
         int counter = 1;
-        while (counter <= n) {
+        while (counter <= n1) {
             sum = sum + counter;
             if (sum % 2 == 0) {
             break;
@@ -25,5 +25,17 @@ public class LoopExercises {
             counter++;
         }
         return sum;
+    }
+    public static void main(String[] args) {
+        LoopExercises exercises = new LoopExercises();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = scanner.nextInt();
+        System.out.println("Sum of numbers from 1 to " + n + " is: " + (exercises.sum(n)));
+        System.out.print("Enter number: ");
+        int n1 = scanner.nextInt();
+        System.out.println("Sum until Even is: " + exercises.sumUntilEven(n1));
+
+
     }
 }
